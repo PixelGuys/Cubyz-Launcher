65055a05b0fc9c31af139732707e7ef4aeb1ce49
IntegratedQuantum <jahe788@gmail.com>
2021-04-16 09:03:15 +0200

    Always write game files into $HOME/.cubyz
    This makes the auto-update feature hard to do, so I had to replace it by an update-prompt.

--- src/io/cubyz/Main.java
+++ src/io/cubyz/Main.java
@@ -1,11 +1,24 @@
 package io.cubyz;
 
+import javax.swing.JOptionPane;
+
 import io.cubyz.github.GitHubConnection;
+import io.cubyz.github.GithubRelease;
 import io.cubyz.ui.MainGUI;
 
 public class Main {
+	public static final String versionString = "0.2.1";
 	public static void main(String[] args) {
-		AutoUpdater.tryUpdate();
+		// Check if a new version is available:
+		try {
+			GithubRelease newest = GitHubConnection.downloadReleaseData("https://api.github.com/repos/PixelGuys/Cubyz-Launcher/releases?per_page=1")[0];
+			if(!newest.version.equals(versionString)) {
+				// Prompt the user to update:
+				JOptionPane.showInternalMessageDialog(null, "A new update of launcher is available.", "information", JOptionPane.INFORMATION_MESSAGE);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		new MainGUI(GitHubConnection.downloadReleaseData("https://api.github.com/repos/PixelGuys/Cubyz/releases?per_page=100"));
 	}
 }
