7411e3980fe7684b35efe3b08167bd072174d98b
OneAvargeCoder193 <mgiakimenko@outlook.com>
2024-06-10 00:13:34 -0400

    Added capy demo

---
+++ build.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+const capy = @import("capy");
+
+pub fn build(b: *std.Build) !void {
+	const target = b.standardTargetOptions(.{});
+	const optimize = b.standardOptimizeOption(.{});
+
+	const exe = b.addExecutable(.{
+		.name = "Cubyz-Launcher",
+		.root_source_file = .{ .path = "src/main.zig" },
+		.target = target,
+		.optimize = optimize,
+	});
+	b.installArtifact(exe);
+
+	const run_cmd = try capy.install(exe, .{ .args = b.args });
+	const run_step = b.step("run", "Run the app");
+	run_step.dependOn(run_cmd);
+
+	// Building for WebAssembly
+	// WebAssembly doesn't have a concept of executables, so the way it works is that we make a shared library and capy exports a '_start' function automatically
+	@setEvalBranchQuota(5000);
+	const wasm = b.addExecutable(.{
+		.name = "Cubyz-Launcher",
+		.root_source_file = .{ .path = "src/main.zig" },
+		.target = b.resolveTargetQuery(
+			comptime std.Target.Query.parse(.{ .arch_os_abi = "wasm32-freestanding" }) catch unreachable,
+		),
+		.optimize = optimize,
+	});
+	const serve = try capy.install(wasm, .{});
+	const serve_step = b.step("serve", "Start a local web server to run this application");
+	serve_step.dependOn(serve);
+
+	const exe_tests = b.addTest(.{
+		.root_source_file = .{ .path = "src/main.zig" },
+		.target = target,
+		.optimize = optimize,
+	});
+
+	const test_step = b.step("test", "Run unit tests");
+	test_step.dependOn(&exe_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const capy = @import("capy");
+
+// This is required for your app to build to WebAssembly and other particular architectures
+pub usingnamespace capy.cross_platform;
+
+pub fn main() !void {
+    try capy.backend.init();
+
+    var window = try capy.Window.init();
+    try window.set(
+        capy.label(.{ .text = "Hello, World", .alignment = .Center }),
+    );
+
+    window.setTitle("Hello");
+    window.setPreferredSize(800, 600);
+    window.show();
+
+    capy.runEventLoop();
+}
